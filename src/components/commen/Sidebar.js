98b70b09d979397c20a84ee4be0845049170a553
loanthiho <loan.hothi24@student.passerellesnumeriques.org>
2023-08-24 16:08:29 +0700

    update sidebar

--- src/components/commen/Sidebar.js
+++ src/components/commen/Sidebar.js
@@ -18,26 +18,33 @@ export const Sidebar = () => {
         <h1 className="mb-5 text-lg font-semibold text-gray-600 mt-5">
           Go Mobile
         </h1>
-        <div className="flex justify-between ">
-          <div className="bg-secondary text-white flex center px-2 py-0.5 rounded-md">
+        <div className="flex">
+          <div
+            className="bg-secondary text-white flex center px-2 py-0.5 rounded-md"
+            style={{ marginRight: "10px" }}
+          >
             <div>
               <AiFillApple size={30} />
             </div>
-            <div className="">
+            <div className="ml-2">
               <span className="text-gray-300 text-[12px]">Download on the</span>
               <h1 className="text-md font-semibold">App Store</h1>
             </div>
           </div>
-          <div className="bg-secondary text-white flex center px-2 py-0.5 rounded-md">
+          <div
+            className="bg-secondary text-white flex center px-2 py-0.5 rounded-md"
+            style={{ marginLeft: "10px" }}
+          >
             <div>
               <IoPlaySharp size={30} />
             </div>
-            <div className="">
+            <div className="ml-2">
               <span className="text-gray-300 text-[12px]">Download on the</span>
               <h1 className="text-md font-semibold">Google Play</h1>
             </div>
           </div>
         </div>
+
         <p className="text-sm mt-3">About Contact Legal Policy</p>
         <span className="text-gray-500 text-[12px]">© Copyright 2022</span>
       </section>
