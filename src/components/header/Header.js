eda8a0c026963a0f81ecd86f5d144223138feb29
loanthiho <loan.hothi24@student.passerellesnumeriques.org>
2023-09-05 11:43:20 +0700

    update img

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,109 @@
+// import React, { useState } from "react";
+// import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+// import { Link, NavLink, useNavigate } from "react-router-dom";
+// import logo from "../assets/images/logo.jpg";
+// import profile from "../assets/images/st.jpg";
+// import { navBar } from "../assets/data/Data";
+
+// export const Header = () => {
+//   const activeNavLink = ({ isActive }) => (isActive ? "active" : "NavLink");
+//   const [isMenu, setIsMenu] = useState(false);
+//   const [isSearchOpen, setIsSearchOpen] = useState(false);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const navigate = useNavigate();
+
+//   const handleLogoClick = () => {
+//     setIsLoading(true);
+//     setTimeout(() => {
+//       navigate("/");
+//       setIsLoading(false);
+//       window.location.reload();
+//     }, 1000);
+
+//     setIsSearchOpen(false);
+//   };
+
+//   const handleSearchClick = () => {
+//     setIsSearchOpen(!isSearchOpen);
+//   };
+
+//   return (
+//     <>
+//       <header className="fixed top-0 left-0 z-50 w-screen h-[8vh] md:shadow-md shadow-sm bg-white">
+//         <div className="hidden md:flex justify-between px-7 p-2">
+//           <div className="logo flex cursor-pointer" onClick={handleLogoClick}>
+//             <div>
+//               <img src={logo} alt="logo" width="40px" height="40px" />
+//             </div>
+//             <h2 className="text-2xl font-semibold ml-3">pluse</h2>
+//           </div>
+//           {/* navlinks */}
+//           <div className="menu">
+//             <ul className="flex">
+//               {navBar.map((list, i) => (
+//                 <li className={`mx-5 py-2  ${activeNavLink}`} key={i}>
+//                   <NavLink to={list.path}>{list.name}</NavLink>
+//                 </li>
+//               ))}
+//             </ul>
+//           </div>
+//           {/* profile */}
+//           <div className="profile flex items-center">
+//             <AiOutlineSearch
+//               size={22}
+//               onClick={handleSearchClick}
+//               className="cursor-pointer"
+//             />
+//             {isSearchOpen && (
+//               <input
+//                 type="text"
+//                 placeholder="Search..."
+//                 className="bg-gray-100 rounded-lg px-2 py-1 ml-2"
+//               />
+//             )}
+//             <button className="bg-primary hover:bg-gray-600 px-6 py-1.5 text-white rounded-full mx-3">
+//               Upload
+//             </button>
+//             <div className="img w-10 h-10 rounded-full hover:ring-2 hover:ring-gray-400">
+//               <img
+//                 src={profile}
+//                 alt="profile"
+//                 className="img w-10 h-10 bg-red-300 rounded-full object-cover cursor-pointer"
+//               />
+//             </div>
+//           </div>
+//         </div>
+//         {/* mobile */}
+//         <div className="flex items-center justify-between md:hidden h-full pl-2 pr-8 ">
+//           <Link to={"/"} className="flex items-center gap-2 ">
+//             <div className="logo flex cursor-pointer" onClick={handleLogoClick}>
+//               <div>
+//                 <img src={logo} alt="logo" width="40px" height="40px"></img>
+//               </div>
+//               <h2 className="text-2xl font-semibold ml-3">pluse</h2>
+//             </div>
+//           </Link>
+//           <div>
+//             {isMenu && (
+//               <div className="bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-16 left-0 w-full ">
+//                 <ul className="flex flex-col">
+//                   {navBar.map((list, i) => (
+//                     <li className={`mx-5 py-2  ${activeNavLink}`} key={i}>
+//                       <NavLink to={list.path}>{list.name}</NavLink>
+//                     </li>
+//                   ))}
+//                 </ul>
+//               </div>
+//             )}
+//             <AiOutlineMenu size={20} onClick={() => setIsMenu(!isMenu)} />
+//           </div>
+//         </div>
+//       </header>
+//       {isLoading && <div className="loading-overlay"></div>}
+//     </>
+//   );
+// };
+
 import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
 import { Link, NavLink, useNavigate } from "react-router-dom";
@@ -10,6 +116,7 @@ export const Header = () => {
   const [isMenu, setIsMenu] = useState(false);
   const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [isProfileOpen, setIsProfileOpen] = useState(false); // State for profile menu
   const navigate = useNavigate();
 
   const handleLogoClick = () => {
@@ -25,6 +132,11 @@ export const Header = () => {
 
   const handleSearchClick = () => {
     setIsSearchOpen(!isSearchOpen);
+  };
+
+  // Toggle profile menu
+  const handleProfileClick = () => {
+    setIsProfileOpen(!isProfileOpen);
   };
 
   return (
@@ -64,15 +176,34 @@ export const Header = () => {
             <button className="bg-primary hover:bg-gray-600 px-6 py-1.5 text-white rounded-full mx-3">
               Upload
             </button>
-            <div className="img w-10 h-10 rounded-full hover:ring-2 hover:ring-gray-400">
-              <img
-                src={profile}
-                alt="profile"
-                className="img w-10 h-10 bg-red-300 rounded-full object-cover cursor-pointer"
-              />
+            {/* Toggle profile menu */}
+            <div className="relative group">
+              <div
+                className="img w-10 h-10 rounded-full hover:ring-2 hover:ring-gray-400 cursor-pointer"
+                onClick={handleProfileClick}
+              >
+                <img
+                  src={profile}
+                  alt="profile"
+                  className="img w-10 h-10 bg-red-300 rounded-full object-cover"
+                />
+              </div>
+              {/* Profile menu */}
+              {isProfileOpen && (
+                <div className="absolute right-0 mt-2 bg-white shadow-lg rounded-lg">
+                  <ul className="py-2">
+                    <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                      <Link to="/profile">View Profile</Link>
+                    </li>
+                    <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                      <Link to="/settings">Settings</Link>
+                    </li>
+                    {/* Add more menu items as needed */}
+                  </ul>
+                </div>
+              )}
             </div>
           </div>
-        </div>
         {/* mobile */}
         <div className="flex items-center justify-between md:hidden h-full pl-2 pr-8 ">
           <Link to={"/"} className="flex items-center gap-2 ">
@@ -97,6 +228,7 @@ export const Header = () => {
             )}
             <AiOutlineMenu size={20} onClick={() => setIsMenu(!isMenu)} />
           </div>
+        </div>
         </div>
       </header>
       {isLoading && <div className="loading-overlay"></div>}
